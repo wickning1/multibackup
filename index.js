b30a7f9ef0bbb6277ad17776d277b90641ef5b7e
Nick Wing <git@mythgaming.net>
2018-10-20 19:08:45 -0500

    untested first draft

---
+++ index.js
@@ -0,0 +1,33 @@
+const Site = require('lib/site.js')
+const Source = require('lib/source.js')
+
+const config = {
+  sources: [
+    '/mnt/storage'
+  ],
+  sites: [{
+    redundancy: 2,
+    servers: [{
+      user: 'ubuntumedia',
+      host: '192.168.0.48',
+      keyfile: '~/.ssh/id_rsa',
+      disks: [{
+        path: '/media/storage/backups'
+      }]
+    }]
+  }]
+}
+const source = new Source(config.sources)
+const sites = config.sites.map(siteconfig => new Site(siteconfig))
+
+(async function () {
+  await source.gather()
+  await Promise.all(sites.map(async function (site) {
+    await site.connect()
+    await site.snapshot()
+    await site.gather()
+    await site.delete(source)
+    await site.put(source)
+  }))
+  process.exit()
+})()
