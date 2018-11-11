bf844d05f7617cdceb7f267b6c6db1141e37169c
wickning1 <git@mythgaming.net>
2018-11-11 17:43:00 -0600

    move bytesToHuman to utility module and change index to test it

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Site = require('./lib/site')
 const MultiSource = require('./lib/multisource')
 const Output = require('./lib/output')
+const util = require('./lib/util')
 
 async function main() {
   const config = require('./config.js')
+  for (i = 0; i < 10; i++) {
+    console.log(i, util.bytesToHuman(i))
+  }
+  process.exit()
   const source = new MultiSource(config.sources)
   await source.gather()
 
