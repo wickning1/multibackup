ba4b5db282c5e510f47c9428c94784330af75199
wickning1 <git@mythgaming.net>
2018-11-18 10:15:25 -0600

    allow gathering on both sides to happen in parallel

--- index.js
+++ index.js
@@ -9,16 +9,21 @@ async function main() {
 
   const config = require('./config.js')
   const source = new MultiSource(config.sources)
-  await source.gather()
-
   const sites = config.sites.map(siteconfig => new Site(siteconfig, source))
+  await Promise.all([
+    source.gather(),
+    ...sites.map(async function (site) {
+      await site.connect()
+      await site.snapshot()
+      await site.gather()
+    })
+  ])
+
   await Promise.all(sites.map(async function (site) {
-    await site.connect()
-    await site.snapshot()
-    await site.gather()
     await site.delete()
     await site.put()
   }))
+
   clearInterval(timer)
   process.exit()
 }
